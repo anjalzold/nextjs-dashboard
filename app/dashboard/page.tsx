5ddc114152a304c896eb0cbbf35a9bfa16a04754
anj <78712815+alankgit@users.noreply.github.com>
2023-11-25 09:09:47 +0545

    asa

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function page() {
-  return <div>THis is dashboard page</div>;
+  return (
+    <div>
+      THis is dashboard page
+      <h1>hey aj</h1>
+    </div>
+  );
 }
